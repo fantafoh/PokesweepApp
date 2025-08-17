6ef2af37b4d069ea07da47b7848576faa3aae7d2
fantafoh <dents-clumps6a@icloud.com>
2025-08-17 17:00:17 -0400

    Update scrapeCard.js

--- api/scrapeCard.js
+++ api/scrapeCard.js
@@ -1,1 +1,10 @@
+export default function handler(req, res) {
+  const { cardName } = req.query;
 
+  if (!cardName) {
+    return res.status(400).json({ error: "Please provide a cardName" });
+  }
+
+  // For now, just echo the card name
+  res.status(200).json({ cardName });
+}
