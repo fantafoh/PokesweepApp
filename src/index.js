a789969f3b1b9575739f6c7220f9d5198aa501b1
fantafoh <dents-clumps6a@icloud.com>
2025-09-16 18:33:34 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx"; // explicitly include .jsx
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
